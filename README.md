6a024d71ae6f51847582214ad4c239c4b2380add
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-01 12:16:42 +0900

    setup env

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # threejs-handle
+
+## dev
+
+```sh
+# start
+npx webpack-dev-server
+
+# build
+npx webpack
+```
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8000,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })], // templateを指定しないとhtmlにrootが入らない
+};
